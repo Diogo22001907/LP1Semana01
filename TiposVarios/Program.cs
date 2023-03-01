5bd75da84dcf3e8e4893d0d5cbbec563eeb2de46
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-01 22:06:14 +0000

    Add integer variables

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22;
+            uint height = 180;
+            short year = 2023;
+            ushort price = 54000;
+            long CarPrice = 13000000;
+            ulong HousePrice = 25000500;
+            byte weight = 130;
+            sbyte size = 11;
+
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(year);
+            Console.WriteLine(price);
+            Console.WriteLine(CarPrice);
+            Console.WriteLine(HousePrice);
+            Console.WriteLine(weight);
+            Console.WriteLine(size);
+        }
+    }
+}
