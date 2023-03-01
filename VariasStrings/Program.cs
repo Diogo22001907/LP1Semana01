682ce518bf096c5798a45ace0caee97016072f7b
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-01 23:53:42 +0000

    Added strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Diogo Miguel Viegas Ferreira";
+            string citation = "and i quote \"This was is last wish\"";
+            string citation2 = @"and i quote ""This was is last wish""";
+            string filePath = "C:\\Users\\Diogo\\Documents\\LP1";
+            string fileLocation = @"C:\User\Diogo\Documents\LP1";
+            string monkeyEmoji = "\u1F648 monkey covering is eyes ";
+
+
+
+            Console.WriteLine(myName);
+            Console.WriteLine(citation);
+            Console.WriteLine(citation2);
+            Console.WriteLine(filePath);
+            Console.WriteLine(fileLocation);
+            Console.WriteLine(monkeyEmoji);
+
+
+        }
+    }
+}
