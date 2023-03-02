b400cc630059d167f1e1aec38d6b8076d2b95e64
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-02 14:29:17 +0000

    Added the exercise 6 strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,6 +6,15 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            double xx = 0.12345;
+            int ii = 18;
+
+            string firstEx = String.Format("'{0:f2}' e '{0:p1}'", xx);
+            string secondEx = String.Format("'0x{0:X}' e '{0:C}'", ii);
+
+            Console.WriteLine(firstEx);
+            Console.WriteLine(secondEx);
+
             string myName = "Diogo Miguel Viegas Ferreira";
             string citation = "and i quote \"This was is last wish\"";
             string citation2 = @"and i quote ""This was is last wish""";
@@ -31,17 +40,20 @@ namespace VariasStrings
             string t = String.Format("Eu {0} de beber {1} Monsters ao dia !", "gosto", 4);
             string u = String.Format("O meu nome é {0} {1} tenho {2} e vivo em {3}", "Diogo", "Ferreira", 23, "Lisboa");
             string f = $"My name is {name} {lastName} and i am {age} years old";
-            int x = 5;
+            int z = 5;
             int y = 10;
-            int q = x + y;
+            int q = z + y;
             
-            Console.WriteLine($@"se é x = {x} e y = {y} então x + y = {q}");
+            Console.WriteLine($@"se é x = {z} e y = {y} então x + y = {q}");
             Console.WriteLine(t);
             Console.WriteLine(s);
             Console.WriteLine(u);
             Console.WriteLine(f);
             Console.WriteLine("Olá o meu nome é {0} {1} e tenho {2} anos", name, lastName, age);
             Console.WriteLine(@"\u1F355 é o emoji da pizza");
+
+
+
 
 
         }
