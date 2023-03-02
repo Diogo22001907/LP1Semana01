2353ae8345af02ed17ba2306159b7f2e0d058f64
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-02 13:59:05 +0000

    Added more strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -23,6 +23,27 @@ namespace VariasStrings
             Console.WriteLine(monkeyEmoji);
 
 
+            string name = "Diogo";
+            string lastName = "Ferreira";
+            string age = "23";
+
+            string s = "O meu nome é " + name + " " + lastName + " e tenho " + age + " anos";
+            string t = String.Format("Eu {0} de beber {1} Monsters ao dia !", "gosto", 4);
+            string u = String.Format("O meu nome é {0} {1} tenho {2} e vivo em {3}", "Diogo", "Ferreira", 23, "Lisboa");
+            string f = $"My name is {name} {lastName} and i am {age} years old";
+            int x = 5;
+            int y = 10;
+            int q = x + y;
+            
+            Console.WriteLine($@"se é x = {x} e y = {y} então x + y = {q}");
+            Console.WriteLine(t);
+            Console.WriteLine(s);
+            Console.WriteLine(u);
+            Console.WriteLine(f);
+            Console.WriteLine("Olá o meu nome é {0} {1} e tenho {2} anos", name, lastName, age);
+            Console.WriteLine(@"\u1F355 é o emoji da pizza");
+
+
         }
     }
 }
