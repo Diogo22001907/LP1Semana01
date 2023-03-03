ce04f660d3eafb9b25c6848006bf3808e55f7378
Diogo Ferreira <a22001907@alunos.ulht.pt>
2023-03-03 11:44:54 +0000

    Exercise 7 completed

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                int num;
+                if (int.TryParse(arg, out num))
+                {
+                    Console.WriteLine("The input given : " + num + " is a integer and is Hexadecimal value is '0x{0:X}'", num);
+                    
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("The input given : " + arg + " is not an integer");
+                }
+
+
+            }
+        }
+    }
+}
